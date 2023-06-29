2a1fee38a8b97fd18fcbfbeeeb00b6edaa6958b5
seoseongmin96 <justin090452@gmail.com>
2023-06-29 20:10:36 +0900

    complete

--- project/distance/distance.py
+++ project/distance/distance.py
@@ -3,12 +3,10 @@ import threading
 import time
 from datetime import datetime
 
-line = '',
-
-   
+import pandas as pd 
 
 ser = serial.Serial(
-        port= '/dev/ttyUSB0',
+        port= '/dev/ttyUSB1',
         baudrate= 57600,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
@@ -17,36 +15,35 @@ ser = serial.Serial(
 )
 
 
-
-alivethread = True
-
-
-
 def inch2cm(num):
-    return int(num) * 2.54
+    return round(int(num) * 2.54, 2)
 
 def readthread(low, high):  
+    df = pd.DataFrame(columns=["time", "distance", "Exists"])
     for i in range(low, high):
-        
         data = ser.read(size=1)
         if data == b'R':
             data = ser.read(size=3)
-            saved_data = datetime.now().strftime("%Y-%m-%d %H:%M:%S"), f"{inch2cm(data)}cm"
-            
-            
-            
+            distance = (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), f"{inch2cm(data.decode())}cm")
+            row = {"distance": distance[1], "time" : distance[0]}
+
         elif data == b'P':
             data = ser.read(size=1)
             if data == b'1':
-                print(f"{saved_data[0]}, {saved_data[1]}, 있음")
-                
+                row["Exists"] = "있음"
+                print(f"{distance[0]}, {distance[1]}, 있음")
             elif data == b'0':
-                print(f"{saved_data[0]}, {saved_data[1]}, 없음")
+                row["Exists"] = "없음"
+                print(f"{distance[0]}, {distance[1]}, 없음")
+
+            df.loc[len(df)] = row
+
+            df.to_csv('distance.csv', index=False, mode='w')
             
-
-
 thread = threading.Thread(target=readthread, args=(1, 10000))
 thread.start()
+
+
 
 # def main():
 #     while True:
---
+++ project/distance/distance2.py
@@ -0,0 +1,76 @@
+import serial
+import threading
+import time
+from datetime import datetime
+import csv
+import pandas as pd 
+
+
+
+   
+
+ser = serial.Serial(
+        port= '/dev/ttyUSB1',
+        baudrate= 57600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1 
+)
+
+
+
+alivethread = True
+
+
+
+
+def inch2cm(num):
+    return round(int(num) * 2.54, 2)
+
+def readthread(low, high):  
+    with open('distance.csv', 'w', encoding='utf-8') as f:
+        for i in range(low, high):
+            data = ser.read(size=1)
+            if data == b'R':
+                data = ser.read(size=3)
+                saved_data = datetime.now().strftime("%Y-%m-%d %H:%M:%S"), f"{inch2cm(data)}cm"
+
+            elif data == b'P':
+                data = ser.read(size=1)
+                if data == b'1':
+                    f.write(f"{saved_data[0]}, {saved_data[1]}, 있음 \n")
+                    print(f"{saved_data[0]}, {saved_data[1]}, 있음")
+                    
+                elif data == b'0':
+                    f.write(f"{saved_data[0]}, {saved_data[1]}, 없음 \n")
+                    print(f"{saved_data[0]}, {saved_data[1]}, 없음")
+                
+
+
+thread = threading.Thread(target=readthread, args=(1, 10000))
+thread.start()
+
+
+
+# def main():
+#     while True:
+#         #print("Hello")
+#         time.sleep(1)
+# main()
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
