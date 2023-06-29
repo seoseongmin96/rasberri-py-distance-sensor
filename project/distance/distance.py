c0f0a5bb01e4db53a1ee0435218e409a76421f56
seoseongmin96 <justin090452@gmail.com>
2023-06-29 17:03:32 +0900

    fix code

--- project/distance/distance.py
+++ project/distance/distance.py
@@ -1,6 +1,7 @@
 import serial
 import threading
 import time
+from datetime import datetime
 
 line = '',
 
@@ -30,23 +31,28 @@ def readthread(low, high):
         data = ser.read(size=1)
         if data == b'R':
             data = ser.read(size=3)
-            print(f"측정거리 : {inch2cm(data)}cm")
-
-        if data == b'P':
+            saved_data = datetime.now().strftime("%Y-%m-%d %H:%M:%S"), f"{inch2cm(data)}cm"
+            
+            
+            
+        elif data == b'P':
             data = ser.read(size=1)
-            print("boolean 값 :" , data)
+            if data == b'1':
+                print(f"{saved_data[0]}, {saved_data[1]}, 있음")
+                
+            elif data == b'0':
+                print(f"{saved_data[0]}, {saved_data[1]}, 없음")
+            
 
 
-        
-        
 thread = threading.Thread(target=readthread, args=(1, 10000))
 thread.start()
 
-def main():
-    while True:
-        #print("Hello")
-        time.sleep(1)
-main()
+# def main():
+#     while True:
+#         #print("Hello")
+#         time.sleep(1)
+# main()
 
 
 
